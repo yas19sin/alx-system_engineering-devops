b99106663930ec5ead341d023574a3c12ca03c7f
yas19sin <yas19sin@gmail.com>
2024-04-24 02:51:11 +0100

    done

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Puppet Install and configure nginx web server
+
+package { 'nginx':
+  ensure => present,
+}
+
+file_line { 'redirect_to':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://worldsim.nousresearch.com permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
