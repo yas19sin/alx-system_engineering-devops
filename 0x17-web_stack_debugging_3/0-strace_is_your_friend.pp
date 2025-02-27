68ac70d2b040332bbe116486336523089406d4b1
yas19sin <yas19sin@gmail.com>
2025-02-27 03:32:20 +0000

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet manifest to fix typo in wp-settings.php causing Apache 500 error
-exec { 'fix_wordpress_typo':
-  command => 'sed -i "s/\.phpp/\.php/g" /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/usr/bin', '/bin'],
-  onlyif  => 'grep -q "\.phpp" /var/www/html/wp-settings.php',
+
+exec { 'wordpress-settings.php fix':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
