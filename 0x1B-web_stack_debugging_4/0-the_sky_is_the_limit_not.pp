f475d1878adcd2590a863ac9ed3824a33a0ef5b3
yas19sin <yas19sin@gmail.com>
2025-03-07 06:23:05 +0000

    lint fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# Increase Nginx request handling capacity by raising the max open files limit.  
+# Increase Nginx request handling capacity by raising the max open files limit.
 
-exec { 'Increase Nginx max open files limit':  
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',  
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',  
+exec { 'Increase Nginx max open files limit':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
